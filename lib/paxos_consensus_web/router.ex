546b100a179a454ffe738750fc4c4c26a4691c79
Fly Dev <dev@fly.io>
2025-06-28 17:28:46 +0000

    replace static route with LiveView dashboard

--- lib/paxos_consensus_web/router.ex
+++ lib/paxos_consensus_web/router.ex
@@ -17,7 +17,7 @@ defmodule PaxosConsensusWeb.Router do
   scope "/", PaxosConsensusWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", DashboardLive
   end
 
   # Other scopes may use custom stacks.
